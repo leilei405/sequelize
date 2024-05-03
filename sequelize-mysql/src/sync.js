c912f28be14ee9596d54e60be1728585273f445b
fll <fllning@163.com>
2024-05-03 13:55:06 +0800

    feat: 同步数据模型到数据库

---
+++ sequelize-mysql/src/sync.js
@@ -0,0 +1,21 @@
+const seq = require("./db");
+
+// 需要同步的模型
+require("./model");
+
+// 测试连接
+seq
+  .authenticate()
+  .then(() => {
+    console.log("连接成功");
+  })
+  .catch((err) => {
+    console.log("连接失败", err);
+  });
+
+// 同步数据库
+// force: true 强制同步，删除原表
+seq.sync({ force: true }).then(() => {
+  console.log("数据库同步成功");
+  process.exit();
+});
